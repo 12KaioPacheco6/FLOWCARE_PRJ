f7e86c89e99ab2ccf62be6a1bc752078f4e547a9
Nina Bastos <bastosnina91@gmail.com>
2025-06-14 18:13:42 -0300

    Alterações

--- paginas/cadastro-produto/includes/cadastrar-produto.inc.php
+++ paginas/cadastro-produto/includes/cadastrar-produto.inc.php
@@ -2,6 +2,7 @@
 
 class Produto 
 {
+    private $id; // Adicionei um ID para seguir o formato do banco de dados
     private $nome;
     private $marca;
     private $local;
@@ -9,56 +10,84 @@ class Produto
     private $preco;
     private $descricao;
 
-    public function __construct($nome, $marca, $local, $categoria, $preco, $descricao)
+    //construtor da classe Produto
+    // function __construct($nome = "", $marca = "", $local = "", $categoria = "", $preco = 0.0, $descricao = "")
+    // {
+    //     $this->nome       = $nome;
+    //     $this->marca      = $marca;
+    //     $this->local      = $local;
+    //     $this->categoria  = $categoria;
+    //     $this->preco      = $preco;
+    //     $this->descricao  = $descricao;
+    // }
+
+    //implementar o método que recebe os dados do formulário e insere esses dados em cada atributo do objeto
+    function receberDadosDoFormulario($conexao)
     {
-        $this->nome = $nome;
-        $this->marca = $marca;
-        $this->local = $local;
-        $this->categoria = $categoria;
-        $this->preco = $preco;
-        $this->descricao = $descricao;
+        //AVISO: cuidado ao receber dados de um formulário e enviá-los ao banco de dados. Se o seu código não contiver os comandos apropriados, o servidor estará sujeito ao tipo de invasão conhecido como INJEÇÃO DE SQL
+        $this->nome         = trim($conexao->escape_string($_POST["nome"]));
+        $this->marca        = trim($conexao->escape_string($_POST["marca-produto"]));
+        $this->local        = trim($conexao->escape_string($_POST["local"]));
+        $this->categoria    = trim($conexao->escape_string($_POST["categoria"]));
+        $this->preco        = trim($conexao->escape_string($_POST["preco"]));
+        $this->descricao    = trim($conexao->escape_string($_POST["descricao"]));
     }
 
-    function getMarca(){
-        return $this->marca;
-    }
-    function setMarca($marca){
-        $this->marca = $marca;
-    }
-
-    function getNome(){
-        return $this->nome;
-    }
-    function setNome($nome){
-        $this->nome = $nome;
-    }
-
-    function getLocal(){
-        return $this->local;
-    }
-    function setLocal($local){
-        $this->local = $local;
+    //método que cadastra os dados do objeto aluno no banco de dados
+    function cadastrar($conexao, $nomeDaTabela3)
+    {
+        // Coloquei o id como NULL, pois ele é auto-incremento no banco de dados
+    $sql = "INSERT $nomeDaTabela3 VALUES(
+            '$this->nome',
+            '$this->marca',
+            '$this->local',
+            '$this->categoria',
+            '$this->preco',
+            '$this->descricao'
+            )";
+    $conexao->query($sql) or die($conexao->error);
     }
 
-    function getCategoria(){
-        return $this->categoria;
-    }
-    function setCategoria($categoria){
-        $this->categoria = $categoria;
-    }
+    // function getMarca(){
+    //     return $this->marca;
+    // }
+    // function setMarca($marca){
+    //     $this->marca = $marca;
+    // }
 
-    function getPreco(){
-        return $this->preco;
-    }
-    function setPreco($preco){
-        $this->preco = $preco;
-    }
+    // function getNome(){
+    //     return $this->nome;
+    // }
+    // function setNome($nome){
+    //     $this->nome = $nome;
+    // }
 
-    function getDescricao(){
-        return $this->descricao;
-    }
-    function setDescricao($descricao){
-        $this->descricao = $descricao;
-    }
+    // function getLocal(){
+    //     return $this->local;
+    // }
+    // function setLocal($local){
+    //     $this->local = $local;
+    // }
+
+    // function getCategoria(){
+    //     return $this->categoria;
+    // }
+    // function setCategoria($categoria){
+    //     $this->categoria = $categoria;
+    // }
+
+    // function getPreco(){
+    //     return $this->preco;
+    // }
+    // function setPreco($preco){
+    //     $this->preco = $preco;
+    // }
+
+    // function getDescricao(){
+    //     return $this->descricao;
+    // }
+    // function setDescricao($descricao){
+    //     $this->descricao = $descricao;
+    // }
 }
 ?>
--- paginas/cadastro-produto/php/cadastrar-produto.php
+++ paginas/cadastro-produto/php/cadastrar-produto.php
@@ -1,11 +1,10 @@
 <?php
-  require_once "../includes/paginas/cadastro-produto/includes/cadastrar-produto.inc.php";
-  require_once "../includes/paginas/cadastro-produto/includes/banco-de-dados-produto.inc.php";
-
-  
-
+  // Ajustei o caminho do arquivo para incluir corretamente
+  require_once "../includes/cadastrar-produto.inc.php";
+  require_once "../includes/banco-de-dados-produto.inc.php";
   //criar o objeto banco de dados que, nesse momento, armazena a conexão com o servidor, inicializando o construtor da nossa classe
-  $banco = new BancoDeDados("localhost", "root", "");
+  //Passando o endereço do servidor, usuário, senha, nome do banco de dados e os nomes das tabelas que serão criadas
+  $banco = new BancoDeDados("localhost", "root", "", "flowcare", "categoria", "marca", "produto", "comentario");
 
   //criar a conexão física com o servidor MySQL
   $conexao = $banco->criarConexao();
@@ -20,18 +19,18 @@
   $banco->definirCharset($conexao);
 
   //invocando o método para criar a tabela 
-  $banco->criarTabela($conexao);
+  $banco->criarTabelaProduto($conexao);
 
   //a partir desse ponto no arquivo principal, vamos criar o objeto Cadastrar Produtos e invocar os métodos da classe cadastrar-produto.inc.php
   $produto = new Produto();
 
   //para cadastrarmos o dados do objeto Cadastrar Produtos no banco, precisamos fazer com o PHP teste se o botão de cadastro foi acionado no formulário
-if(isset($_POST["cadastrar"])) {
-   $produto->receberDadosDoFormulario($conexao);
-   $produto->cadastrar($conexao, $banco->produto);
-   echo "<p> Produtos foram cadastrados com sucesso no banco de dados. </p>";
-}
+  if(isset($_POST["cadastrar-produto"])) { // Coloquei o nome do botão de cadastro do formulário, estava "cadastrar"
+    $produto->receberDadosDoFormulario($conexao);
+    $produto->cadastrar($conexao, $banco->nomeDaTabela3);
+    echo "<p> Produtos foram cadastrados com sucesso no banco de dados. </p>";
+  }
 
   //após finalizar toda a execução da nossa aplicação, "matamos" a conexão com o MySQL
   $banco->desconectar($conexao);
- ?>                       
+?>                    
